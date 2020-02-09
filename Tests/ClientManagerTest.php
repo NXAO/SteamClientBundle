012925c6b7e71ec00f86bdc375d6fa90eedf834f
Oleg Charnyshevich <admin@love4dev.ru>
2020-02-09 21:51:37 +0300

    Update ClientManagerTest.php

--- Tests/ClientManagerTest.php
+++ Tests/ClientManagerTest.php
@@ -4,11 +4,28 @@ declare(strict_types=1);
 
 namespace Nxao\Component\SteamClient\Tests;
 
+use Nxao\Component\SteamClient\ClientManager;
+use Nxao\Component\SteamClient\Clients\SteamApi;
 use PHPUnit\Framework\TestCase;
 
-class ClientManagerTest extends TestCase
+class ClientManagerTest extends \Symfony\Bundle\FrameworkBundle\Test\KernelTestCase
 {
+    public $clientManager;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        static::bootKernel();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->clientManager = null;
+    }
+
     public function testExecude(){
+        $this->clientManager->execute(SteamApi::class);
         $this->assertEmpty([]);
     }
 }
