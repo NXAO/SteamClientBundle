8d615ab01f00d14554f56361bad78cb152d73add
Oleg Charnyshevich <admin@love4dev.ru>
2020-02-09 20:31:13 +0300

    Set up DependencyInjection

--- DependencyInjection/Compiler/WriterRegistryPass.php
+++ DependencyInjection/Compiler/WriterRegistryPass.php
@@ -6,10 +6,33 @@ namespace Nxao\Component\SteamClient\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\RuntimeException;
 
 class WriterRegistryPass implements CompilerPassInterface
 {
+    private string $handlerTag;
+
+    public function __construct(string $handlerTag = 'messenger.message_handler')
+    {
+        $this->handlerTag = $handlerTag;
+    }
+
     public function process(ContainerBuilder $container)
     {
+        $this->registerHandlers($container);
+    }
+
+    private function registerHandlers(ContainerBuilder $container): void
+    {
+        foreach ($container->findTaggedServiceIds($this->handlerTag, true) as $serviceId => $tags) {
+            foreach ($tags as $tag) {
+                $className = $container->getDefinition($serviceId)->getClass();
+                $r = $container->getReflectionClass($className);
+
+                if (null === $r) {
+                    throw new RuntimeException(sprintf('Invalid service "%s": class "%s" does not exist.', $serviceId, $className));
+                }
+            }
+        }
     }
 }
--- DependencyInjection/NxaoSteamClientExtension.php
+++ DependencyInjection/NxaoSteamClientExtension.php
@@ -4,20 +4,28 @@ declare(strict_types=1);
 
 namespace Nxao\Component\SteamClient\DependencyInjection;
 
+use Nxao\Component\SteamClient\AnnotationHandlers\AnnotationHandlerInterface;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class NxaoSteamClientExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator('@NxaoSteamClient/Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+
+        $this->addAnnotatedClassesToCompile([
+            '**\\Clients\\',
+        ]);
+
+        $container->registerForAutoconfiguration(AnnotationHandlerInterface::class)
+            ->addTag('nxao.steam_client.annotation_handlers');
     }
 }
